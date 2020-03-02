4aa0d59a964f7ae06f31b38798e14a2b33469808
Remy Guts <remyguts@gmail.com>
2020-03-01 18:22:32 -0800

    create burger controller js and connect and export

---
+++ db/models/controllers/burgers_controller.js
@@ -0,0 +1,31 @@
+"use strict";
+
+const orm = require("../config/orm.js");
+
+const burger = {
+  selectAll: cb => {
+    orm.selectAll("burgers", res => {
+      cb(res);
+    });
+  },
+
+  insertOne: (cols, vals, cb) => {
+    orm.insertOne("burgers", cols, vals, res => {
+      cb(res);
+    });
+  },
+
+  updateOne: (objColVals, condition, cb) => {
+    orm.updateOne("burgers", objColVals, condition, res => {
+      cb(res);
+    });
+  },
+
+  deleteOne: (condition, cb) => {
+    orm.deleteOne("burgers", condition, res => {
+      cb(res);
+    });
+  }
+};
+
+module.exports = burger;
