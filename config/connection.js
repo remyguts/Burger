11e8380437013e636944944714e4df5547c875b1
Remy Guts <remyguts@gmail.com>
2020-03-01 19:29:56 -0800

    changed port

--- config/connection.js
+++ config/connection.js
@@ -7,7 +7,7 @@ if (process.env.JAWSDB_URL) {
 } else {
   connection = mysql.createConnection({
     host: "localhost",
-    port: 3306,
+    port: "process.env.PORT",
     user: "root",
     password: "otis1978",
     database: "burgersDB"
