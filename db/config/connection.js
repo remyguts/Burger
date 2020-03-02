057fa53bb418f0e0e006de82e86caf6ee371403a
Remy Guts <remyguts@gmail.com>
2020-03-01 19:10:19 -0800

    connection js update

--- db/config/connection.js
+++ db/config/connection.js
@@ -1,4 +1,21 @@
 "use strict";
-
 const mysql = require("mysql");
 let connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "MyPassword",
+    database: "burgersDB"
+  });
+}
+
+connection.connect(err => {
+  if (err) throw err;
+});
+
+module.exports = connection;
